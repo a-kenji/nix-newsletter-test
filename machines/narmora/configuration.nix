5b491f6dca6423d5d88ac8ba01cae4f915c527b6
a-kenji <aks.kenji@protonmail.com>
2024-11-07 22:26:33 +0100

    keys: add all phaer keys

--- machines/narmora/configuration.nix
+++ machines/narmora/configuration.nix
@@ -6,11 +6,12 @@
 {
   clan.core.networking.targetHost = "root@188.245.150.72";
   clan.core.sops.defaultGroups = [
-      "admins"
+    "admins"
   ];
 
   imports = [
     ./disko.nix
+    ./ssh.nix
     ../../modules/hebbot
     inputs.srvos.nixosModules.server
     inputs.srvos.nixosModules.hardware-hetzner-cloud
@@ -20,9 +21,4 @@
     inputs.clan.clanModules.root-password
   ];
 
-  users.users.root.openssh.authorizedKeys.keys = [
-    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOu1koO8pJ6t0I+rpSVfjD1m6eDk9KTp8cvGL500tsQ9" #kenji
-    "sk-ssh-ed25519@openssh.com AAAAGnNrLXNzaC1lZDI1NTE5QG9wZW5zc2guY29tAAAAIHqRNv8hueRuN4khLUQMiPVS0NqwZfX17BNXIRZJ9yRPAAAAE3NzaDpoZWxsb0BwaGFlci5vcmc=" #phaer
-  ];
-  services.openssh.settings.PermitRootLogin = "yes";
 }
