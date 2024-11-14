06be4500e8d57d2cde3697a475c1b5d481eea5cf
a-kenji <aks.kenji@protonmail.com>
2024-11-14 10:31:48 +0100

    hebbot: add initial configuration

--- modules/hebbot/default.nix
+++ modules/hebbot/default.nix
@@ -3,6 +3,10 @@
   lib,
   ...
 }:
+  let usual_reporters = [
+      "@a-kenji:matrix.org"
+  ];
+in
 {
   imports = [
     ./vars.nix
@@ -32,8 +36,131 @@
         "@a-kenji:matrix.org"
       ];
       #TODO: These are user facing, have them in a toplevel directory structure
-      sections = [ ];
-      projects = [ ];
+      sections = [
+        {
+          emoji = "💻";
+          name = "nixos";
+          title = "nixos";
+          description = "";
+          order = 40;
+          inherit usual_reporters;
+        }
+        {
+          emoji = "📦";
+          name = "nixpkgs";
+          title = "nixpkgs";
+          description = "";
+          order = 50;
+          inherit usual_reporters;
+        }
+        {
+          emoji = "⚖️";
+          name = "rfcs";
+          title = "rfcs";
+          description = "The Nix community RFCs";
+          website = "https://github.com/NixOS/rfcs";
+          order = 100;
+          inherit usual_reporters;
+        }
+        {
+          emoji = "🏘";
+          name = "nix-community";
+          title = "nix-community";
+          description = "Nix community projects";
+          order = 100;
+          inherit usual_reporters;
+        }
+        {
+          emoji = "🏗";
+          name = "external projects";
+          title = "external projects";
+          description = "External Nix projects";
+          order = 100;
+          inherit usual_reporters;
+        }
+        {
+          emoji = "📅";
+          name = "events";
+          title = "events";
+          description = "";
+          order = 500;
+          inherit usual_reporters;
+        }
+        {
+          emoji = "📺";
+          name = "media";
+          title = "media";
+          description = "";
+          order = 500;
+          inherit usual_reporters;
+        }
+      ];
+      projects = [
+        {
+          emoji = "❄️";
+          name = "Nix";
+          title = "The Nix Package Manager";
+          description = "";
+          website = "";
+          default_section = "nixos";
+        }
+        {
+          emoji = "📝";
+          name = "documentation team";
+          title = "documentation team";
+          description = "Nixpkgs Documentation Team";
+          website = "";
+          default_section = "nixpkgs";
+        }
+        {
+          emoji = "🪩";
+          name = "disko";
+          title = "disko";
+          description = "";
+          website = "";
+          default_section = "nix-community";
+        }
+        {
+          emoji = "🐛";
+          name = "nixos-anywhere";
+          title = "nixos-anywhere";
+          description = "";
+          website = "";
+          default_section = "nix-community";
+        }
+        {
+          emoji = "📢";
+          name = "conferences";
+          title = "conferences";
+          description = "Conferences";
+          website = "";
+          default_section = "events";
+        }
+        {
+          emoji = "👥";
+          name = "meetups";
+          title = "meetups";
+          description = "Meetups";
+          website = "";
+          default_section = "events";
+        }
+        {
+          emoji = "🎙";
+          name = "podcasts";
+          title = "podcasts";
+          description = "";
+          website = "";
+          default_section = "media";
+        }
+        {
+          emoji = "📚";
+          name = "books";
+          title = "books";
+          description = "";
+          website = "";
+          default_section = "media";
+        }
+      ];
     };
 
     templates = {
