4993a368a50cc9e8c4b0503d423f1cf1c8e83719
a-kenji <aks.kenji@protonmail.com>
2025-02-02 11:29:32 +0700

    vars: Update to new api

--- modules/hebbot/vars.nix
+++ modules/hebbot/vars.nix
@@ -4,7 +4,7 @@
 {
   clan.core.vars.generators.this-week-in-nix = {
     files.username.secret = false;
-    prompts.password.createFile = true;
-    prompts.username.createFile = true;
+    prompts.password.persist = true;
+    prompts.username.persist = true;
   };
 }
