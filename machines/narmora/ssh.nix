f83eafe1fe187f86e2b629ae53d4b05a2cc9fe45
a-kenji <aks.kenji@protonmail.com>
2025-02-13 13:36:56 +0700

    keys: Add pinpox & hsjobeky ssh keys

--- machines/narmora/ssh.nix
+++ machines/narmora/ssh.nix
@@ -3,8 +3,12 @@
   ...
 }:
 {
-  users.users.root.openssh.authorizedKeys.keys = [
-    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOu1koO8pJ6t0I+rpSVfjD1m6eDk9KTp8cvGL500tsQ9" # kenji
-  ] ++ (builtins.filter (l: l != [ ]) (builtins.split "\n" (builtins.readFile inputs.phaer-keys)));
+  users.users.root.openssh.authorizedKeys.keys =
+    [
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOu1koO8pJ6t0I+rpSVfjD1m6eDk9KTp8cvGL500tsQ9" # kenji
+    ]
+    ++ (builtins.filter (l: l != [ ]) (builtins.split "\n" (builtins.readFile inputs.phaer-keys)))
+    ++ (builtins.filter (l: l != [ ]) (builtins.split "\n" (builtins.readFile inputs.hsjobeki-keys)))
+    ++ (builtins.filter (l: l != [ ]) (builtins.split "\n" (builtins.readFile inputs.pinpox-keys)));
   services.openssh.settings.PermitRootLogin = "yes";
 }
